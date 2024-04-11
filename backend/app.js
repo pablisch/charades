cdad351b9f11e330fa4b3c48c296d87d2fc93a1f
Pablo <pablito@me.com>
2024-04-11 15:48:54 +0100

    Refactored app.js BE.

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');
 require('dotenv').config();
 
 const dbPassword = process.env.MONGODB_PW;
 const dbUser = process.env.MONGODB_USER;
 const dbName = process.env.MONGODB_DB || 'charades_DEV';
 
-// Original site 'gallery' connection string:
 const mongoDbUrl = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.auyhe66.mongodb.net/${dbName}`;
 
 const healthRoutes = require('./routes/health');
@@ -16,31 +14,32 @@ const charadeRoutes = require('./routes/charade');
 
 const app = express();
 
-const corsOrigin = process.env.CORS_ORIGIN || 'https://charades-pthh.onrender.com';
-console.log('🪧  >>> CORS:', corsOrigin);
+// const corsOrigin = process.env.CORS_ORIGIN || 'https://charades-pthh.onrender.com';
+// console.log('🪧  >>> CORS:', corsOrigin);
 
-const corsOptions = {
-  origin: corsOrigin,
-};
+// const corsOptions = {
+//   origin: corsOrigin,
+// };
 
 // app.use(cors(corsOptions));
+
 app.use(cors());
 
-mongoose
-  .connect(mongoDbUrl)
-  .then(() => {
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(mongoDbUrl);
     console.log(
       `🥳 Successfully connected to MongoDB Atlas ${dbName} database! 🌎`
     );
-  })
-  .catch((error) => {
+  } catch (error) {
     console.log(`😖 Unable to connect to MongoDB Atlas ${dbName} database! ❌`);
     console.error(error);
-  });
+  }
+};
+
+connectToDatabase();
 
 app.use(express.json());
-
-app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Charades API!');
